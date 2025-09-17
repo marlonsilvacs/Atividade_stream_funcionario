04f251a6ccbe2bc31f16c9b40a348adf619a1634
marlonsilvacs <164301515+marlonsilvacs@users.noreply.github.com>
2025-09-16 22:04:43 -0300

    add

---
+++ src/View/ProcessadorFuncionarios.java
@@ -0,0 +1,39 @@
+package src.View;
+import src.Model.Funcionario;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ProcessadorFuncionarios {
+
+    public static void main(String[] args) {
+        List<Funcionario> funcionarios = Arrays.asList(
+                new Funcionario("Marlon", 32, 10000.0),
+                new Funcionario("Lorrany", 23, 7500.0),
+                new Funcionario("João André", 40, 15200.0),
+                new Funcionario("Emanuel", 29, 3100.0),
+                new Funcionario("Iago", 25, 2000.0)
+        );
+
+        gerarRelatorio(funcionarios);
+    }
+
+    public static void gerarRelatorio(List<Funcionario> funcionarios) {
+        System.out.println("Funcionários com idade maior que 30 anos:");
+        funcionarios.stream()
+                .filter(f -> f.getIdade() > 30)
+                .forEach(System.out::println);
+
+        System.out.println("\nLista de nomes dos funcionários:");
+        List<String> nomes = funcionarios.stream()
+                .map(Funcionario::getNome)
+                .collect(Collectors.toList());
+        nomes.forEach(System.out::println);
+
+        System.out.println("\nSoma total dos salários:");
+        double somaSalarios = funcionarios.stream()
+                .map(Funcionario::getSalario)
+                .reduce(0.0, Double::sum);
+        System.out.println("R$ " + somaSalarios);
+    }
+}
